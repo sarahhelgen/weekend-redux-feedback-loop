f64bbb0d2d765d7f66c4008e265d458eb26294c9
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-23 17:34:31 -0500

    boilerplate for index.js complete

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+
+TO DO LIST:
+
+[x] npm installs
+[x] create db in postico
+[ ] create drawing/visual of the app
+[x] boilerplate in index.js
+    [x] createStore
+    [x] combineReducers
+    [x] redux logger applyMiddleware
+    [x] Provider and reduxStore as props
+
+Create components:
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 //reducers go here
+
+
+//creating the store:
+const reduxStore = createStore(
+    combineReducers({
+        //reducers go here  
+    }),
+    applyMiddleware(logger)
+);
+
+
+ReactDOM.render(<Provider store={reduxStore}>
+    <App />
+</Provider>,
+    document.getElementById('root'));
+serviceWorker.unregister();
