7a879319a2efe923155738f0330de95291c7d637
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-24 17:00:20 -0500

    server side post route complete

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,3 +2,17 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.post('/', (req,res) =>{
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`;
+    let newFeedback = req.body;
+    console.log('adding new feedback', newFeedback );
+    pool.query( queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+    .then(result =>{
+        res.sendStatus(201);
+    }).catch(error =>{
+        console.log( 'error with POST', error );
+    });
+});
+
+module.exports = router;
