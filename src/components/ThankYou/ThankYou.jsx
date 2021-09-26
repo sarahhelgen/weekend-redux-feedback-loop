0dd40bec0557205aced5c13a172885e1e4dbafd7
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-26 08:00:00 -0500

    more code comments

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -3,7 +3,8 @@ import {useHistory} from 'react-router-dom';
 function ThankYou () {
 
     const history = useHistory();
-
+    //this page displays after a user sends their feedback to the db and
+    //lets the user leave a new set of feedback
     return(
         <>
         <p>Thank You!</p>
